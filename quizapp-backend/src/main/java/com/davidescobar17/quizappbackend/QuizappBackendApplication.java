a45363c069f391d4696c11da235258013ab98f66
davidescobar17 <davidescobar17@outlook.com>
2021-12-31 21:48:31 +1030

    Intialise Spring Boot backend

---
+++ quizapp-backend/src/main/java/com/davidescobar17/quizappbackend/QuizappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.davidescobar17.quizappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizappBackendApplication.class, args);
+	}
+
+}
---
+++ quizapp-backend/src/test/java/com/davidescobar17/quizappbackend/QuizappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidescobar17.quizappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
