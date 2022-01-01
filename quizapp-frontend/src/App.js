16f9871598b0458e741ad4320c71e24f2fe54b4a
davidescobar17 <davidescobar17@outlook.com>
2022-01-01 15:08:07 +1030

    Add react router with homepage, header and footer

--- quizapp-frontend/src/App.js
+++ quizapp-frontend/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import FooterComponent from './components/FooterComponent';
+import HeaderComponent from './components/HeaderComponent';
+import HomePagePromptComponent from './components/HomePagePromptComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <HeaderComponent />
+        <div className= "container">
+          <Switch>
+            <Route exact path = "/" component = {HomePagePromptComponent}></Route>
+          </Switch>
+        </div>
+        <FooterComponent />
+        </Router>
     </div>
   );
 }
---
+++ quizapp-frontend/src/components/HomePagePromptComponent.js
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import QuestionService from '../services/QuestionService'
+
+const ListQuestionComponent = () => {
+
+    const [questions, setQuestions] = useState([])
+
+    useEffect(() => {
+
+        getAllQuestions();
+    }, [])
+
+    const getAllQuestions = () => {
+        QuestionService.getAllQuestions().then((response) => {
+            setQuestions(response.data)
+            console.log(response.data);
+        }).catch(error =>{
+            console.log(error);
+        })
+    }
+
+    return (
+        <div className = "container vertical-center">
+            <div className = "jumbotron m-5">
+                <h1 className = "text-center display-1"> Test your knowledge </h1>
+                <div className="text-center m-5">
+                    <Link to = "/questions" className = "btn btn-primary mx-2" > Manage questions </Link>
+                    <Link to = {{pathname: "/test", state: questions}} className = "btn btn-primary mx-2" > Test myself </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ListQuestionComponent
